5508c6b3c0df51a52dbd243a9ca11aec9b5c8ab2
Evan Goldman <evangoldman11@gmail.com>
2024-10-21 12:23:17 -0700

    adding 3 entries

--- Source/src/app/details.ts
+++ Source/src/app/details.ts
@@ -2,6 +2,7 @@ export interface CardItem {
     title: string,
     description: string,
     category: string,
+    date: string,
     highlighted?: boolean
 }
 
@@ -9,52 +10,62 @@ export const Projects: CardItem[] = [
     {
         title: "YOLO in PyTorch",
         description: "I implemented the YOLO (You Only Look Once) real time object detection model in PyTorch.",
-        category: "Machine Learning"
+        category: "Machine Learning",
+        date: "5/29/2024"
     },
     {
         title: "Minecraft Dataset",
         description: "I made a dataset of 20,000+ screenshots of minecraft with full depth and semantic annotations.",
-        category: "Machine Learning"
+        category: "Machine Learning",
+        date: "10/21/2024"
     },
     {
         title: "Arduino Galactica",
         description: "I made a simple version of Galactica using AVR C++ as the final project for my Embedded Systems class.",
-        category: "Electrical"
+        category: "Electrical",
+        date: "6/9/2024"
     },
     {
         title: "Geometry Dash RL",
         description: "I made a bot that learned to play a simplified version of geometry dash with machine learning.",
-        category: "Machine Learning"
+        category: "Machine Learning",
+        date: "8/4/2023"
     },
     {
         title: "Website",
         description: "I made this website using AngularJS to act as a professional portfolio.",
-        category: "General SW"
+        category: "General SW",
+        date: "9/17/2022"
     },
     {
         title: "Early Games",
         description: "A collection of lower quality games I made when I was still learning the basics of game development.",
-        category: "Game Dev"
+        category: "Game Dev",
+        date: "9/21/2022"
     },
     {
         title: "Mecha Raptor Simulator",
         description: "A Roblox game where you play as a robot velociraptor. This game is still a work in progress.",
-        category: "Game Dev"
+        category: "Game Dev",
+        date: "12/18/2022"
     },
     {
         title: "Piano Tiles Player",
         description: "Python/C# application to get as high of a score in piano tiles as possible.",
-        category: "Game Bot"
+        category: "Game Bot",
+        date: "9/21/2022"
     },
     {
         title: "Chess Player",
         description: "Python application to play chess on chess.com by reading the screen and moving accordingly. Uses Stockfish chess engine.",
-        category: "Game Bot"
+        category: "Game Bot",
+        date: "11/22/2022"
     },
     {
         title: "Jurassic World Player",
         description: "Using Tesseract from google, I made a bot to read the current state of the battle and move accordingly.",
-        category: "Game Bot"
+        category: "Game Bot",
+        date: "9/28/2022"
     },
     // {
     //     title: "Fluidigm Internship",
@@ -69,11 +80,13 @@ export const Projects: CardItem[] = [
     {
         title: "3D Modeling with blender",
         description: "Created various 3d models in blender mainly for use in game development.",
-        category: "Game Dev"
+        category: "Game Dev",
+        date: "11/15/2022"
     },
     {
         title: "Random Map Generator",
         description: "Created random dungeons using cellular automation and perlin noise.",
-        category: "Game Dev"
+        category: "Game Dev",
+        date: "12/18/2022"
     },
 ]
