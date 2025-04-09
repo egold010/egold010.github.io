aa7bee144ad66ca63c9efaf4a7e233902ba2465c
Evan Goldman <evangoldman11@gmail.com>
2025-04-09 08:02:29 -0700

    adding tasl to career

--- Source/src/app/base-components/career/career.component.ts
+++ Source/src/app/base-components/career/career.component.ts
@@ -19,8 +19,17 @@ export class CareerComponent implements OnInit {
       link: 'https://aviatr.ucrhighlanders.org/',
       date: 'Nov 2024 - Present',
       description: 'Manages the computer vision and autonomous navigation subteams (6 members) at UCR\'s drone club.',
-      skills: ['C++', 'Python', 'ROS2'],
+      skills: ['C++', 'Python'],
       image: 'assets/timeline-images/aviatr.jpg',
+    },
+    {
+      title: 'Graduate Research Assistant',
+      company: 'TASL',
+      link: 'https://tasl.ucr.edu/',
+      date: 'Oct 2024 - Present',
+      description: 'Trustworthy Autonomous Systems Laboratory. Advised by Prof. Jiachen Li',
+      skills: ['C++', 'Python', 'ROS2'],
+      image: 'assets/timeline-images/tasl.png',
     },
     {
       title: 'Software Engineer Intern',
